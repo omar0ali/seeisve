7b819e18a6369659252016beeae72d8340274c5b
Omar Ali <61897650+omar0ali@users.noreply.github.com>
2023-07-21 09:13:21 -0400

    fixing design, scrolling horizontally. adding Search options (Equals, contains, larger or less than ..etc)

--- app.js
+++ app.js
@@ -16,12 +16,37 @@ const app = Vue.createApp({
         searchF: function () {
             var vm = this;
             let result = [];
+
+            console.log(this.$refs.option_search.value);
+
             this.searchAtIndex = true;
             for (let i = 0; i < vm.parse_csv.length; i++) {
-                // console.log(this.$refs.key_search.value);
-                // console.log(vm.parse_csv[i][this.$refs.key_search.value]);
-                if (vm.parse_csv[i][this.$refs.key_search.value].toLowerCase().includes(this.$refs.search.value.toLowerCase())) {
-                    result.push(vm.parse_csv[i]);
+                switch (this.$refs.option_search.value.toLowerCase()) {
+                    case "includes":
+                        if (vm.parse_csv[i][this.$refs.key_search.value].toLowerCase().includes(this.$refs.search.value.toLowerCase())) {
+                            result.push(vm.parse_csv[i]);
+                        }
+                        break;
+                    case "equals":
+                        if (vm.parse_csv[i][this.$refs.key_search.value].toLowerCase() === (this.$refs.search.value.toLowerCase())) {
+                            result.push(vm.parse_csv[i]);
+                        }
+                        break;
+                    case "not equals":
+                        if (vm.parse_csv[i][this.$refs.key_search.value].toLowerCase() !== (this.$refs.search.value.toLowerCase())) {
+                            result.push(vm.parse_csv[i]);
+                        }
+                        break;
+                    case "larger than":
+                        if (parseInt(vm.parse_csv[i][this.$refs.key_search.value].toLowerCase()) > parseInt((this.$refs.search.value.toLowerCase()))) {
+                            result.push(vm.parse_csv[i]);
+                        }
+                        break;
+                    case "less than":
+                        if (parseInt(vm.parse_csv[i][this.$refs.key_search.value].toLowerCase()) < parseInt((this.$refs.search.value.toLowerCase()))) {
+                            result.push(vm.parse_csv[i]);
+                        }
+                        break;
                 }
             }
             vm.parse_csv_searched = result;
@@ -35,17 +60,17 @@ const app = Vue.createApp({
             if (vm.parse_header[vm.parse_header.length - 1] === "\r") {
                 vm.parse_header.pop();
             }
-        
+
             for (let i = 1; i < lines.length; i++) {
                 let obj = {};
                 let currentline = lines[i].trim();
                 let cells = [];
                 let inQuotes = false;
                 let cell = "";
-        
+
                 for (let j = 0; j < currentline.length; j++) {
                     let char = currentline[j];
-        
+
                     if (char === "," && !inQuotes) {
                         cells.push(cell);
                         cell = "";
@@ -55,19 +80,19 @@ const app = Vue.createApp({
                         cell += char;
                     }
                 }
-        
+
                 // Add the last cell after the loop ends
                 cells.push(cell);
-        
+
                 for (let j = 0; j < vm.parse_header.length; j++) {
                     obj[vm.parse_header[j]] = cells[j] || ""; // Use empty string for missing cells
                 }
-        
+
                 result.push(obj);
             }
-        
+
             return result; // JavaScript object
-        },loadCSV(e) {
+        }, loadCSV(e) {
             this.searchAtIndex = false;
             let vm = this
             if (window.FileReader) {
